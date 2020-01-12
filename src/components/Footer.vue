381d99bcf88359c2a566b6e3e2818daafe99a01b
pluthin <pontus.lut@gmail.com>
2020-01-12 17:45:23 +0100

    Patch: Footer desktop and mobile

---
+++ src/components/Footer.vue
@@ -0,0 +1,62 @@
+<template>
+  <footer class="footer">
+    <BackToTop visibleoffset="50" bottom="50px" right="50px">
+    <div class="back-to-top-wrapper">
+      <font-awesome-icon icon="arrow-circle-up" class="back-to-top-icon"/>
+      {{ back_to_top_button }}
+    </div>
+    </BackToTop>
+    <ul class="contact-info-wrapper">
+      <li class="contact-info">
+        <img class="schoolflow-footer-logo" src=".././assets/schoolflow_logo.png" alt="">
+      </li>
+      <li v-for="c_info in contact_info " :key="c_info" class="contact-info">
+        {{ c_info.info }}
+      </li>
+    </ul>
+    <div class="copyright-info">{{ copyright_info }}</div>
+    <ul class="footer-menu">
+      <li v-for="link in links" :key="link">
+        <router-link :to="link.path">{{ link.link_title }}</router-link>
+      </li>
+    </ul>
+  </footer>
+</template>
+<script>
+import BackToTop from 'vue-backtotop'
+export default {
+  data() {
+    return {
+      contact_info: [
+        {
+          info: '073-831 44 44'
+        },
+        {
+          info: 'kontakt@schoolflow.se'
+        }
+      ],
+      copyright_info: '© Copyright Schoolflow AB 2019',
+      links: [
+        {
+            path: '/prenumerationer',
+            link_title: 'Prenumerationer'
+        },
+        {
+            path: '/arskurser',
+            link_title: 'Årskurser',
+        },
+        {
+            path: '/amnen',
+            link_title: 'Ämnen',
+        },
+        {
+            path: '/om-oss',
+            link_title: 'Om oss'
+        }
+      ],
+      back_to_top_button: 'Tillbaka till toppen'
+    }
+  }, 
+  components: { BackToTop }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,26 +1,30 @@
+/* -- Vue -- */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+/* -- Components -- */
+import Footer from './components/Footer'
 import Header from './components/Header'
+Vue.component('Header', Header)
+Vue.component('Footer', Footer)
+
+/* -- External -- */
+
+/* Font Awesome */
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { dom } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-
-
-
-Vue.config.productionTip = false
-
-Vue.component('Header', Header)
-
-
 dom.watch()
 library.add(fas)
-
-
 library.add(faSpinner)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+/* End Font Awesome */
+
+Vue.config.productionTip = false
 
 new Vue({
   router, 
