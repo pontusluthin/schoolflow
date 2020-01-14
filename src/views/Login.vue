1542df182d1461363963441ed756362e6eca6c11
pluthin <pontus.lut@gmail.com>
2020-01-14 04:09:14 +0100

    Patch: Login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="page">
-    Login Sida
+  <div class="login page">
+    <div class="login-title">{{ login_title }}</div>
+    <form>
+      <div class="field-wrapper">
+        <label class="login-sub-title">{{ username_title }}</label>
+        <input type="text" class="field">
+      </div>
+
+       <div class="field-wrapper">
+        <label class="login-sub-title">{{ password_title }}</label>
+        <input type="text" class="field">
+      </div>
+
+      <input type="submit" class="login-submit-buttom" value="Logga in">
+
+    </form>
   </div>
 </template>
 <script>
 export default {
-  
+  data () {
+    return {
+      login_title: 'Logga in',
+      username_title: 'Användarnamn', 
+      password_title: 'Lösenord' 
+    }
+  }
 }
 </script>
